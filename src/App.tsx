7990ee545ce33aaf3c06f0bc1b794aa5cab6d6fb
yoko-nori <ykinrtk.gamecreation@gmail.com>
2023-12-10 02:22:16 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "./components/Main";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header style={{ textAlign: "center" }}>
+        <h1>ゆめみ・フロントエンドテスト</h1>
+        <h1>都道府県別人口推移</h1>
+      </header>
+      <Main />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CheckField.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+type Props = {
+  prefectures:
+    | {
+        prefCode: number;
+        prefName: string;
+      }[];
+
+  onChange: (name: string, prefName: number, check: boolean) => void;
+};
+
+const Styles: { [key: string]: React.CSSProperties } = {
+  checkcardList: {
+    display: "flex",
+    flexWrap: "wrap",
+    padding: "10px",
+    justifyContent: "flex-start",
+    justifySelf: "auto",
+  },
+  text: { display: "contents", marginLeft: "1em", cursor: "pointer" },
+  checkcard: {
+    borderRadius: "24px",
+    border: "solid 2px",
+    textAlign: "center",
+    padding: "4px",
+    margin: "0.5rem",
+  },
+};
+
+// 都道府県一覧のチェックボックスを表示するコンポーネント
+const CheckField: React.FC<Props> = ({ prefectures = [], onChange }) => {
+  return (
+    <>
+      <div style={Styles.checkcardList}>
+        {prefectures.map((prefecture) => (
+          <div style={Styles.checkcard} key={prefecture.prefCode}>
+            <input
+              type="checkbox"
+              name="Prefecture name"
+              onChange={(event) =>
+                onChange(
+                  prefecture.prefName,
+                  prefecture.prefCode,
+                  event.target.checked
+                )
+              }
+              id={"checkbox" + prefecture.prefCode}
+            />
+            <label
+              style={Styles.text}
+              htmlFor={"checkbox" + prefecture.prefCode}
+            >
+              {prefecture.prefName.length === 3
+                ? "　" + prefecture.prefName
+                : prefecture.prefName}
+            </label>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CheckField;
